2c342f72a540d4c5a54476ba36cc2570b622a89e
alpaca-tc <alpaca-tc@alpaca.tc>
2018-07-28 20:42:34 +0900

    Setup rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,18 @@
-require "bundler/setup"
-require "mf_formula"
+# frozen_string_literal: true
+
+require 'mf_formula'
+require 'pry'
+
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
